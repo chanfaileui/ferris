0ba2b74f391dd7eadf644d64b7fc323f491b312e
Fai Leui Chan <faileui@gmail.com>
2025-03-07 17:43:27 +1100

    christmas_lights

--- lab03/christmas_tree/src/main.rs
+++ lab03/christmas_tree/src/main.rs
@@ -12,9 +12,8 @@ enum Instruction {
 
 #[derive(Debug)]
 struct Light {
-    // TODO: change me!
-    left: (),
-    right: (),
+    left: Option<Box<Light>>,
+    right: Option<Box<Light>>,
     brightness: i32,
 }
 
@@ -24,10 +23,78 @@ fn get_instructions_from_stdin() -> VecDeque<Instruction> {
     ron::from_str(&instructions).unwrap()
 }
 
+fn sum_and_count(light: &Light) -> (i32, i32) {
+    let left = if let Some(left) = &light.left {
+        sum_and_count(left)
+    } else {
+        (0, 0)
+    };
+    let right = if let Some(right) = &light.right {
+        sum_and_count(right)
+    } else {
+        (0, 0)
+    };
+    (left.0 + right.0 + light.brightness, left.1 + right.1 + 1)
+}
+
 fn main() {
     let instructions = get_instructions_from_stdin();
-    let light = Light { left: (), right: (), brightness: 0};
-    println!("{instructions:?}");
-    println!("{light:?}");
-    // TODO: your implementation here
+    let mut light = Light {
+        left: None,
+        right: None,
+        brightness: 0,
+    };
+    
+    // println!("{instructions:?}");
+
+    // you're holding the root node
+    // you need some sort of current pointer
+    // you keep moving the pointer
+
+    let mut curr = &mut light;
+    for instruction in instructions {
+        match instruction {
+            Instruction::Set(value) => {
+                curr.brightness = value;
+            }
+            Instruction::Left => {
+                match curr.left {
+                    None => {
+                        curr.left = Some(Box::new(Light {
+                            left: None,
+                            right: None,
+                            brightness: 0,
+                        }));
+                        curr = curr.left.as_mut().unwrap();
+                        // or
+                        // curr = curr.left.as_mut().and_then(|boxed| Some(boxed.as_mut()));
+                    }
+                    Some(ref mut left_light) => {
+                        curr = left_light;
+                    }
+                }
+            }
+            Instruction::Right => match curr.right {
+                None => {
+                    curr.right = Some(Box::new(Light {
+                        left: None,
+                        right: None,
+                        brightness: 0,
+                    }));
+                    curr = curr.right.as_mut().unwrap();
+                }
+                Some(ref mut right_light) => {
+                    curr = right_light;
+                }
+            },
+            Instruction::Reset => {
+                curr = &mut light;
+            }
+        }
+    }
+
+    let (sum, count) = sum_and_count(&light);
+    let avg = sum / count;
+    println!("{avg}");
+    // println!("{light:?}");
 }
