52ee106adf93e1814754d6c75e62d79f972c09b6
Fai Leui Chan <faileui@gmail.com>
2025-03-30 23:01:44 +1100

    sequential in progress

--- ortalab/src/game.rs
+++ ortalab/src/game.rs
@@ -24,6 +24,7 @@ impl GameState {
         &self.explain_steps
     }
 
+    /// Returns a HashMap mapping each rank to the number of cards with that rank in played cards
     fn group_rank(&self) -> HashMap<ortalib::Rank, usize> {
         let rank_counts = self
             .round
@@ -33,6 +34,8 @@ impl GameState {
             .counts();
         rank_counts
     }
+
+    /// Returns a HashMap mapping each rank to the cards with that rank in played cards
     fn group_by_rank(&self) -> HashMap<Rank, Vec<&Card>> {
         self.round
             .cards_played
@@ -40,6 +43,10 @@ impl GameState {
             .into_group_map_by(|card| card.rank)
     }
 
+    /// Returns a HashMap mapping each rank to the number of cards with that rank in played cards
+    ///
+    /// # Returns
+    /// * HashMap<Rank, usize> - Maps ranks to their counts
     fn group_suit(&self) -> HashMap<ortalib::Suit, usize> {
         let suit_counts = self
             .round
@@ -49,11 +56,34 @@ impl GameState {
             .counts();
         suit_counts
     }
+
+    /// Returns a HashMap mapping each suit to the number of cards with that suit in played cards
+    ///
+    /// # Returns
+    /// * HashMap<Suit, Vec<&Card>> - Maps suits to vectors of cards with that suit
     fn group_by_suit(&self) -> HashMap<Suit, Vec<&Card>> {
         self.round
             .cards_played
             .iter()
             .into_group_map_by(|card| card.suit)
+    }
+
+    fn is_sorted(&self) -> bool {
+        let cards: &Vec<Card> = &self.round.cards_played;
+        cards.windows(2).all(|w| w[0].rank < w[1].rank)
+    }
+
+    fn is_sequential(&self) -> bool {
+        if !self.is_sorted() {
+            return false;
+        }
+        let cards: &Vec<Card> = &self.round.cards_played;
+        let first_value: f64 = cards[0].rank.rank_value();
+
+        cards
+            .iter()
+            .enumerate()
+            .all(|(i, card)| card.rank.rank_value() == first_value + i as f64)
     }
 
     fn identify_hand(&self) -> PokerHand {
@@ -72,13 +102,36 @@ impl GameState {
         // 1. Are all 5 cards the same rank?
         if rank_count.len() == 1 {
             // are they the same suit?
-            todo!()
             if suit_count.len() == 1 {
-
+                return PokerHand::FlushFive;
+            } else {
+                return PokerHand::FiveOfAKind;
             }
         }
 
-        todo!()
+        // 2. Are all 5 cards the same suit?
+        if suit_count.len() == 1 {
+            // Check if sequential
+            if self
+                .round
+                .cards_played
+                .iter()
+                .map(|card| card.rank)
+                .sorted()
+                .collect::<Vec<_>>()
+                == self
+                    .round
+                    .cards_played
+                    .iter()
+                    .map(|card| card.rank)
+                    .sorted()
+                    .collect::<Vec<_>>()
+            {
+                return PokerHand::StraightFlush;
+            } else {
+                // check if 3 + 2 pattern
+            }
+        }
     }
 
     fn scoring(&self) {
@@ -87,8 +140,7 @@ impl GameState {
 
     pub fn score(&self) -> (Chips, Mult) {
         let poker_hand: PokerHand = self.identify_hand();
-        self.scoring();
         let score: (Chips, Mult) = poker_hand.hand_value();
-        self.scoring();
+        score
     }
 }
