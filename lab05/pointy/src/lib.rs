e76333c2a1cea5d887626ce1deee068478a33f17
Fai Leui Chan <faileui@gmail.com>
2025-04-07 18:56:26 +1000

    point

--- lab05/pointy/src/lib.rs
+++ lab05/pointy/src/lib.rs
@@ -10,10 +10,8 @@
 /// let list = vec!["a", "b", "c"];
 /// assert_eq!(&"a", first(&list));
 /// ```
-// TODO: implement the below function
-// You will need to change its signature
-pub fn first() {
-    todo!("implement the first function");
+pub fn first<T>(points: &[T]) -> &T {
+    &points[0]
 }
 
 /// A generic point struct.
@@ -31,12 +29,12 @@ pub fn first() {
 /// assert_eq!(1.0, point.x);
 /// assert_eq!(2.0, point.y);
 /// ```
-pub struct Point {
-    pub x: i32,
-    pub y: i32,
+pub struct Point<T> {
+    pub x: T,
+    pub y: T,
 }
 
-impl Point {
+impl Point<f32> {
     /// Returns the distance between two points.
     /// ```
     /// use pointy::Point;
@@ -47,10 +45,18 @@ impl Point {
     ///
     /// ```
     /// use pointy::Point;
-    /// let point1 = Point::new(1.0, 2.0);
+    /// let point1 = Point { x: 1.0, y: 2.0 };
     /// assert_eq!(1.0, point1.x);
     /// ```
-    pub fn distance(&self, second: &Point) -> f32 {
-        todo!()
+    pub fn distance(&self, second: &Point<f32>) -> f32 {
+        let x = second.x - self.x;
+        let y = second.y - self.y;
+        (x * x + y * y).sqrt()
+    }
+}
+
+impl<T> Point<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
     }
 }
