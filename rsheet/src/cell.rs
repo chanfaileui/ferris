20e72523d7770988e552886c84cbce478ae4db46
Fai Leui Chan <faileui@gmail.com>
2025-04-17 03:48:32 +1000

    Part 1 ing

--- rsheet/src/cell.rs
+++ rsheet/src/cell.rs
@@ -1,15 +1,15 @@
 use rsheet_lib::cell_value::CellValue;
 
 pub struct Cell {
-    pub expr: Option<String>, // have to call CellExpr to evaluate
+    // pub expr: CellExpr, // have to call CellExpr to evaluate
     pub value: CellValue,
     pub timestamp: u64, // used to prevent older updates overwriting newer ones
 }
 
 impl Cell {
-    pub fn new(expr: Option<String>, value: CellValue) -> Self {
+    pub fn new(value: CellValue) -> Self {
         Self {
-            expr,
+            // expr,
             value,
             timestamp: std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
--- rsheet/src/lib.rs
+++ rsheet/src/lib.rs
@@ -1,12 +1,12 @@
 use cell::Cell;
-use env_logger::fmt::Timestamp;
-use rsheet_lib::cell_expr::CellExpr;
+use rsheet_lib::cell_expr::{CellArgument, CellExpr};
 use rsheet_lib::command::Command;
 use rsheet_lib::connect::{
     Connection, Manager, ReadMessageResult, Reader, WriteMessageResult, Writer,
 };
 use rsheet_lib::replies::Reply;
 use spreadsheet::Spreadsheet;
+use std::collections::HashMap;
 use std::error::Error;
 use std::sync::{Arc, RwLock};
 use std::thread;
@@ -57,59 +57,48 @@ where
                 // implementation for parsing the get and set commands. This is just a
                 // demonstration of how to use msg.parse::<Command>, you may want/have to
                 // change this code.
-                let reply = match msg.parse::<Command>() {
+                let maybe_reply: Option<Reply> = match msg.parse::<Command>() {
                     Ok(command) => match command {
                         Command::Get { cell_identifier } => {
                             let sheet = spreadsheet.read().unwrap();
                             let val = sheet.get(&cell_identifier);
-                            match val {
-                                Some(val) => Reply::Value(format!("{:?}", cell_identifier), val.to_string()),
-                                None => Reply::Error("Cell not found".into()),
-                            }
-                        },
+                            Some(Reply::Value(format!("{:?}", cell_identifier), val))
+                        }
                         Command::Set {
                             cell_identifier,
                             cell_expr,
                         } => {
-                            let sheet = spreadsheet.write().unwrap();
-                            let cell_expr = CellExpr::new(&cell_expr).evaluate();
- 
-                            let cell = Cell {
-                                expr: match cell_expr {
-                                    Ok(val) => Some(val.to_string()),
-                                    Err(_) => None,
-                                },
-                                value: match cell_expr {
-                                    Ok(val) => val,
-                                    Err(_) => return Err(Box::new(std::io::Error::new(
-                                        std::io::ErrorKind::InvalidData,
-                                        "Failed to evaluate cell expression",
-                                    ))),
-                                },
-                                timestamp: std::time::SystemTime::now()
-                                    .duration_since(std::time::UNIX_EPOCH)
-                                    .unwrap()
-                                    .as_secs(),
-                            };
-                            sheet.set(cell_identifier.clone(), cell);
-                            return Ok(());
-                        },
+                            let mut sheet = spreadsheet.write().unwrap();
+                            let cell_expr = CellExpr::new(&cell_expr);
+
+                            let variables: HashMap<String, CellArgument> = HashMap::new();
+
+                            match cell_expr.evaluate(&variables) {
+                                Ok(value) => {
+                                    sheet.set(cell_identifier, Cell::new(value));
+                                    None
+                                }
+                                Err(e) => Some(Reply::Error(format!("Evaluation error: {:?}", e))),
+                            }
+                        }
                     },
-                    Err(e) => Reply::Error(e),
+                    Err(e) => Some(Reply::Error(format!("Evaluation error: {:?}", e))),
                 };
 
-                match send.write_message(reply) {
-                    WriteMessageResult::Ok => {
-                        // Message successfully sent, continue.
-                    }
-                    WriteMessageResult::ConnectionClosed => {
-                        // The connection was closed. This is not an error, but
-                        // should terminate this connection.
-                        break;
-                    }
-                    WriteMessageResult::Err(e) => {
-                        // An unexpected error was encountered.
-                        return Err(Box::new(e));
+                if let Some(reply) = maybe_reply {
+                    match send.write_message(reply) {
+                        WriteMessageResult::Ok => {
+                            // Message successfully sent, continue.
+                        }
+                        WriteMessageResult::ConnectionClosed => {
+                            // The connection was closed. This is not an error, but
+                            // should terminate this connection.
+                            break;
+                        }
+                        WriteMessageResult::Err(e) => {
+                            // An unexpected error was encountered.
+                            return Err(Box::new(e));
+                        }
                     }
                 }
             }
